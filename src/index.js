7a5878a8adddeedcc808e4a048de2cfc7701cc29
sh1var3ddy <srajamuri@gmail.com>
2025-01-09 13:37:16 -0500

    Added dbConnect call

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const fastify = require('fastify')({logger:true});
 
 const serverConfig  = require('./config/serverConfig');
-const app = require('./app')
+const app = require('./app');
+const connectToDB = require('./config/db.config');
 
 fastify.register(app);
 
-fastify.listen({port:serverConfig.PORT},(err)=>{
+fastify.listen({port:serverConfig.PORT},async (err)=>{
     if(err){
         fastify.log.error(err);
         process.exit(1);
     }
+    await connectToDB
     console.log(`Server up at port ${serverConfig.PORT}`);
 })
